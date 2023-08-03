08d9590a561bb7177a1c8130aa4aa30f68569ce8
Abdur-Rahman <abbadshaf05@gmail.com>
2023-08-03 23:36:19 +0100

    updated workers

--- app/entry.worker.ts
+++ app/entry.worker.ts
@@ -1,6 +1,6 @@
 /// <reference lib="WebWorker" />
 
-import { PrecacheHandler } from "./sw";
+import { CacheFirst, NetworkFirst, PrecacheHandler, matchRequest } from "./sw";
 
 // import createStorageRepository from "./database.js";
 
@@ -12,6 +12,37 @@ const DATA = "data-cache";
 const ASSETS = "assets-cache";
 
 // create lru-cache stores here (custom ofc) and pass them instead to the handlers
+
+const documentHandler = new NetworkFirst({
+  cacheName: PAGES,
+});
+
+const loadersHandler = new NetworkFirst({
+  cacheName: DATA,
+  isLoader: true,
+});
+
+const assetsHandler = new CacheFirst({
+  cacheName: ASSETS,
+});
+
+export const defaultFetchHandler = ({ context, request }: any) => {
+  const type = matchRequest(request);
+
+  if (type === "asset") {
+    return assetsHandler.handle(request);
+  }
+
+  if (type === "loader") {
+    return loadersHandler.handle(request);
+  }
+
+  if (type === "document") {
+    return documentHandler.handle(request);
+  }
+
+  return context.fetchFromServer();
+};
 
 const messageHandler = new PrecacheHandler({
   dataCacheName: DATA,
@@ -27,6 +58,11 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
 
+let a = true;
+
 self.addEventListener("message", (event) => {
-  event.waitUntil(messageHandler.handle(event));
+  if (a) {
+    a = false;
+    event.waitUntil(messageHandler.handle(event));
+  }
 });
