dc0ec7fc07999c8e0b7cab3619af02eec64db50c
Abdur-Rahman <abbadshaf05@gmail.com>
2023-08-03 23:35:56 +0100

    chore: created my own `json` function

--- app/sw/utils/request.ts
+++ app/sw/utils/request.ts
@@ -1,40 +1,18 @@
-export type JSONOptions = ResponseInit
+export type JsonFunction = <Data>(
+  data: Data,
+  init?: number | ResponseInit
+) => Response;
 
-export const json = (data: any, options: JSONOptions = {}) => {
-  const headers = new Headers({
-    "Content-Type": "application/json",
-  });
+export const json: JsonFunction = (data, init = {}) => {
+  let responseInit = typeof init === "number" ? { status: init } : init;
+
+  let headers = new Headers(responseInit.headers);
+  if (!headers.has("Content-Type")) {
+    headers.set("Content-Type", "application/json; charset=utf-8");
+  }
 
   return new Response(JSON.stringify(data), {
-    ...options,
-    headers: {
-      ...options.headers,
-      ...headers,
-    },
-    statusText: options.statusText || "OK",
-    status: options.status || 200,
+    ...responseInit,
+    headers,
   });
-}
-
-export declare class DeferredData {
-  private pendingKeysSet;
-  private controller;
-  private abortPromise;
-  private unlistenAbortSignal;
-  private subscribers;
-  data: Record<string, unknown>;
-  init?: ResponseInit;
-  deferredKeys: string[];
-  constructor(data: Record<string, unknown>, responseInit?: ResponseInit);
-  private trackPromise;
-  private onSettle;
-  private emit;
-  subscribe(fn: (aborted: boolean, settledKey?: string) => void): () => boolean;
-  cancel(): void;
-  resolveData(signal: AbortSignal): Promise<boolean>;
-  get done(): boolean;
-  get unwrappedData(): {};
-  get pendingKeys(): string[];
-}
-export type DeferFunction = (data: Record<string, unknown>, init?: number | ResponseInit) => DeferredData;
-export declare const defer: DeferFunction;
+};
